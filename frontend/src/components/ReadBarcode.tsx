b3a96d411a372214827d7358de9b85f5c9b12692
Ryosuke Uchiyama <uchi63rs@yahoo.co.jp>
2025-03-05 00:34:10 +0900

    update

--- frontend/src/components/ReadBarcode.tsx
+++ frontend/src/components/ReadBarcode.tsx
@@ -6,16 +6,11 @@ import { Scanner, IDetectedBarcode } from '@yudiel/react-qr-scanner';
 
 const ReadBarcode: React.FC = () => {
   const navigate = useNavigate();
-  const [scanResult, setScanResult] = useState({ format: '', rawValue: '' });
 
   const handleScan = (results: IDetectedBarcode[]) => {
     if (results.length > 0) {
       for (const item of results) {
         if (item.rawValue.startsWith("978") || item.rawValue.startsWith("979")) {
-          setScanResult({
-            format: results[0].format,
-            rawValue: results[0].rawValue,
-          });
           console.log(item);
           navigate('/book');
         }
